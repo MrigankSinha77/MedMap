98339b4b3565593de2ba2a8b99408e6640c64408
prajwal.agrawal.btech2025 <prajwal.agrawal.btech2025@sitpune.edu.in>
2026-01-23 05:42:40 +0000

    changed the logo

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { Pill, LayoutDashboard } from "lucide-react";
+import { Syringe, LayoutDashboard } from "lucide-react";
 import { ThemeToggle } from "./theme-toggle";
 import { Button } from "@/components/ui/button";
 
@@ -11,7 +11,7 @@ export function Header() {
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="mr-4 flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
-            <Pill className="h-6 w-6 text-primary" />
+            <Syringe className="h-6 w-6 text-primary" />
             <span className="font-bold">MedMap</span>
           </Link>
         </div>
